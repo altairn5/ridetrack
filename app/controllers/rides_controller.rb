a4bc5ff03e206dccfaa075317fab3dd898255782
Humberto Calero <altairn5@uga.edu>
2016-01-25 18:54:50 -0800

    routes and controllers

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,6 +2,9 @@ class RidesController < ApplicationController
 before_action :right_user
 
   def index
+    @current_user = current_user
+    @rides = Ride.all
+    binding.pry
 
   end
   
@@ -15,6 +18,7 @@ before_action :right_user
   	@ride = Ride.find(params[:id])
   	@origin = @ride[:origin]
   	@destination = @ride[:destination]
+    @ride_name = @ride[:ride_name]
   end
 
 
@@ -22,6 +26,7 @@ before_action :right_user
     @user = current_user
     ride_params = params.require(:ride).permit(:origin, :destination, :created_at, :updated_at, :origin_latitude, :origin_longitude ,:destination_latitude ,:destination_longitude ,:ride_name)
   	@ride = Ride.new(ride_params)
+    binding.pry
     @ride.user = current_user
   	if @ride.save 
   		redirect_to ride_path(@ride.id)
@@ -29,5 +34,17 @@ before_action :right_user
   	redirect_to new_ride_path
   	end
   end
+
+  def edit
+    @ride = Ride.find(params[:id])
+  redit edit    
+  end
+
+
+  def update
+
+
+  end
+
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+ 
+ def controller?(*controller)
+    controller.include?(params[:controller])
+  end 
+
+  def action?(*action)
+    @current_action = action.include?(params[:action])
+  end
+
+  def current_page
+  	current_page?(params[:action]) 
+  end
+
 end
